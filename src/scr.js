2a34fd7522a70d5ff3ccfbe7520b7cb1dd6611a3
Natan Santolo <natan.santolo@mercadolibre.com>
2011-03-17 12:15:05 -0300

    Fix when an array is passed it execute the callback only one time.

--- src/scr.js
+++ src/scr.js
@@ -1,48 +1,79 @@
 /*
  *      scr.js
  *      Little tiny loader for javascript sources.
- *      0.1.1 - March 14 2011
+ *      0.1.2 - March 17 2011
  * 
  *      scr.js("http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js", function() {
  *          $("#demo").html("jQuery Loaded from CDN");
  *      });
  */
-var scr={js: function(s, callback) {
-    
-    if (typeof s === "string") {
-        s = [s]; // force array type
-    }
 
-    // Grab the script tag element on the document
-    var script = document.getElementsByTagName("script")[0];
-    
-    var t = s.length;
-    // For each source
-    for (var i = 0; i < t; i++) {
-        // Create a new script tag element
-        var newScript = document.createElement("script");
-        // define the load event to append the new script tag to the document
-            // IE
-            newScript.onreadystatechange = function() {
-                if (newScript.readyState === 'loaded' || newScript.readyState === 'complete') {
-                    newScript.onreadystatechange = null;
-                    if (callback) {
-                        callback();
+(function(d){
+
+var scr = window.scr = (function() {
+        
+        return {
+ 
+            js: function(s, c) {
+
+                if (typeof s === "string") {
+                    s = [s]; // force array type
+                }
+                
+                // Grab the script tag element on the document
+                var script = d.getElementsByTagName("script")[0];
+                // Loader buffer
+                var b = { t: 0, i: 0, r: function() { return b.t === b.i } };
+                // Callback function
+                var callback = function() {  
+                    // increment script loaded
+                    b.i++;
+        
+                    if (c&&b.r()) {
+                        c();
+                    } 
+                }
+                // Ready function
+                var ready = (function() { 
+                    // Feature detection for IE
+                    if (script.readyState) {
+                        return function(n) {
+                            n.onreadystatechange = function() {
+                                if (n.readyState === 'loaded' || n.readyState === 'complete') {
+                                    n.onreadystatechange = null;
+                                        callback();
+                                }
+                            }
+          
+                        }
+                    // For others
+                    } else {
+                        return function(n) {
+                            n.onload = function() {
+                                callback();
+                            }
+                        }
                     }
-                }
-            }
-        
-            // others
-            newScript.onload = function() {
-                if (callback) {
-                    callback();
-                }
-            }
-        
-        // define the source of new script tag element
-        newScript.src = s[i];
-        script.parentNode.insertBefore(newScript,script);
-        
+                    
+                })();
+                                   
+                var t = b.t = s.length;
+                var i = 0;
+                var e = d.createElement("script");
 
-    }
-}}
+                // For each source
+                for (i; i < b.t; i++) {
+                    // define the load event to append the new script tag to the document
+                    var n = e.cloneNode(true);
+                    ready(n);
+                    // define the source of new script tag element
+                    n.src = s[i];
+                    script.parentNode.insertBefore(n,script);
+                    
+                }   
+            }
+        }
+        
+    })();
+
+})(document);
